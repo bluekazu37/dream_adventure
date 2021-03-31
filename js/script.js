19c2e6ebb5448a30b10160cb54085f49bdd0d659
bluekazu37 <81511353+bluekazu37@users.noreply.github.com>
2021-03-31 23:21:19 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,11 +28,13 @@ $(window).scroll(function (){
     fadeAnime();/* アニメーション用の関数を呼ぶ*/
 });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
+//ハンバーガーメニュー
 function toggleNav() {
     const body = document.body;
     const hamburger = document.getElementById("js-hamburger");
     const overlay = document.getElementById("js-overlay");
     const sideMenu_li = document.getElementById("js-sideMenu_li");
+    const registration = document.getElementById("js-registration");
     hamburger.addEventListener("click", function () {
       body.classList.toggle("nav-open");
     });
@@ -42,30 +44,25 @@ function toggleNav() {
     sideMenu_li.addEventListener("click", function () {
         body.classList.remove("nav-open");
       });
+    registration.addEventListener("click", function () {
+        body.classList.remove("nav-open");
+      });
     }
   toggleNav();
 
-//初回のみモーダルをすぐ出す判定。flagがモーダル表示のstart_open後に代入される
-var access = $.cookie('access')
-if(!access){
-    flag = true;
-    $.cookie('access', false);
-}else{
-    flag = false	
-}
-
-//モーダル表示
-$(".modal-open").modaal({
-start_open:flag, // ページロード時に表示するか
-overlay_close:true,//モーダル背景クリック時に閉じるか
-before_open:function(){// モーダルが開く前に行う動作
-    $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
-},
-after_close:function(){// モーダルが閉じた後に行う動作
-    $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
-}
+//モーダルログイン画面
+$(function(){
+    $('.js-modal-open').on('click',function(){
+        $('.js-modal').fadeIn();
+        return false;
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
 });
 
+//ボタンの上にくると表示
 tippy('.cap', {
     placememt: 'top-start',
     animation: 'shift-toward-subtitle',
@@ -82,17 +79,6 @@ $('.title').on('click', function(){ //タイトル要素をクリックしたら
         $(this).addClass('close'); //クラス名closeを付与
     }
 });
-
-//ページが読み込まれた際にopenクラスがついていたら開く動作
-/* $(window).on('load', function(){
-    $('.accordion-area li:first-of-type section').addClass("open");
-    $(".open").each(function(index, element){
-        var Title =$(element).children('.title');
-        $(Title).addClass('close');
-        var Box =$(element).children('box');
-        $(Box).slideDown(500);
-    });
-}); */
 
 $(function(){
 	$('.toggle_title').click(function(){
